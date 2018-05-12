36fa3f37a82b0f848829621cfd96d2e1527d1730
xuyingshao <shaoxuying@yahoo.com>
2018-05-12 13:29:55 -0400

    add user.sql

--- src/main/java/com/example/CourseManager/repositories/UserRepository.java
+++ src/main/java/com/example/CourseManager/repositories/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.CourseManager.models.User;
 
-public interface UserRepository extends CrudRepository<User, Integer>{
+public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
